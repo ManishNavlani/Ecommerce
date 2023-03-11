2a17ced4ce39d601a3f389b47d5165643e9b4868
Manish Navlani <navlanimanish77@outlook.com>
2023-03-11 15:19:57 +0530

    deploy to cyclic

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URL, (err) => {
-  if (!err) {
-    console.log("MongoDB Connection Succeeded.");
-  } else {
-    console.log("Error in DB connection: " + err);
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection: " + err);
+    }
   }
-});
+);
