6befd61582b4a8a3f820ce1a275537d5f19dc4d5
Manish Navlani <navlanimanish77@outlook.com>
2023-03-11 18:58:49 +0530

    fixed errors

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const adminRouter = require("./routers/admin");
-const userRouter = require("./routers/users");
-const productRouter = require("./routers/product");
-const orderRouter = require("./routers/order");
-const cartRouter = require("./routers/cart");
-const stripeRouter = require("./routers/stripe");
-const reviewRouter = require("./routers/review");
+const adminRouter = require("./src/routers/admin");
+const userRouter = require("./src/routers/users");
+const productRouter = require("./src/routers/product");
+const orderRouter = require("./src/routers/order");
+const cartRouter = require("./src/routers/cart");
+const stripeRouter = require("./src/routers/stripe");
+const reviewRouter = require("./src/routers/review");
 dotenv.config();
 
-require("./db/mongoose");
+require("./src/db/mongoose");
 
 const express = require("express");
 const app = express();
@@ -27,11 +27,11 @@ app.use("/api/v4", stripeRouter);
 app.use("/api/v4", reviewRouter);
 app.use("/api/v4", adminRouter);
 
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "../client/dist/index.html"),
+    path.join(__dirname, "./client/dist/index.html"),
     function (err) {
       res.status(500).send(err);
     }
